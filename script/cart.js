d6cbb61624e8e572c84ef2cc11f43a75395beadb
Patric Ronge <pronge@gmail.com>
2020-01-27 20:21:13 +0100

    new json structure, added cart.js

---
+++ script/cart.js
@@ -0,0 +1,23 @@
+let cart = {
+  products: []
+};
+
+const addToCart = e => {
+  const clickedProduct = e.parentElement.querySelector(".product-card__name")
+    .textContent;
+
+  localObj.products.forEach(item => {
+    if (item.name.includes(clickedProduct)) {
+      cart.products.push(item);
+    }
+  });
+};
+
+const getTotalPrice = () => {
+  let price = 0;
+  cart.products.forEach(item => {
+    price += item.price;
+  });
+
+  return price;
+};
--- script/renderProducts.js
+++ script/renderProducts.js
@@ -8,32 +8,46 @@ const productShoes = document.querySelector(
   ".products__shoes .products__card-container"
 );
 
+const lobalDb = {
+  products: []
+};
+
 const cardTemplate = (name, price, img) => {
   return `
     <div class="products__product-card">
     <div class="product-card__image"></div>
     <h3 class="product-card__name">${name}</h3>
     <p class="product-card__price">${price}kr</p>
-    <button type="submit" class="product-card__buy-btn">
+    <button type="submit" class="product-card__buy-btn" onclick="addToCart(this)">
       Add to cart
     </button>
     </div>
     `;
 };
 
-fetch("./products.json")
-  .then(res => {
+async function getProducts() {
+  return fetch("./products.json").then(res => {
     return res.json();
-  })
-  .then(data => {
-    console.log(data);
-    data.shirts.forEach(item => {
-      productShirts.innerHTML += cardTemplate(item.name, item.price);
-    });
-    data.hats.forEach(item => {
-      productHats.innerHTML += cardTemplate(item.name, item.price);
-    });
-    data.shoes.forEach(item => {
-      productShoes.innerHTML += cardTemplate(item.name, item.price);
-    });
   });
+}
+
+getProducts().then(data => {
+  console.log(data);
+
+  data.products.forEach(item => {
+    lobalDb.products.push(item);
+    console.log(item);
+
+    switch (item.category) {
+      case "shirts":
+        productShirts.innerHTML += cardTemplate(item.name, item.price);
+        break;
+      case "hats":
+        productHats.innerHTML += cardTemplate(item.name, item.price);
+        break;
+      case "shoes":
+        productShoes.innerHTML += cardTemplate(item.name, item.price);
+        break;
+    }
+  });
+});
