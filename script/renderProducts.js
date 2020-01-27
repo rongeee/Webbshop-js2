c1e75a50cc3b12e492d923d6142841d037416029
Samuel Mårtensson <samuelmartensson@Samuels-MacBook-Pro.local>
2020-01-27 15:32:02 +0100

    render item script

--- script/renderProducts.js
+++ script/renderProducts.js
@@ -1,1 +1,25 @@
 const productContainer = document.querySelector(".products__container");
+console.log(productContainer);
+
+fetch("./products.json")
+  .then(res => {
+    return res.json();
+  })
+  .then(data => {
+    console.log(data);
+    data.shirts.forEach(item => {
+      productContainer.innerHTML += `
+      <div class="products__product-card">
+      <div class="product-card__image"></div>
+      <h2 class="product-card__name">${item.name}</h2>
+      <p class="product-card__price">${item.price}kr</p>
+      <button type="submit" class="product-card__buy-btn">
+        Add to cart
+      </button>
+    </div>
+        `;
+    });
+    for (let i = 0; i < data.length; i++) {
+      console.log("jasjsa");
+    }
+  });
