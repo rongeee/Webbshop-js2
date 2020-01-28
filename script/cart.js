db504c47db4df2f4c9d0699be52e64c6579a7ccf
Patric Ronge <pronge@gmail.com>
2020-01-28 13:04:12 +0100

    Added find products in db function

--- script/cart.js
+++ script/cart.js
@@ -2,15 +2,32 @@ let cart = {
   products: []
 };
 
+const findProduct = (name, db) => {
+  let foundProduct = db.products.filter(item => {
+    if (name === item.name) {
+      return true;
+    }
+  });
+
+  // Checks if filter-function above returned true and if there is something in the first position
+  if (foundProduct && foundProduct[0]) {
+    return foundProduct[0];
+  } else {
+    console.log("Product not found.");
+    return "Not Found";
+  }
+};
+
 const addToCart = e => {
   const clickedProduct = e.parentElement.querySelector(".product-card__name")
     .textContent;
+  const productInDb = findProduct(clickedProduct, localDb);
 
-  localDb.products.forEach(item => {
-    if (item.name.includes(clickedProduct)) {
-      cart.products.push(item);
-    }
-  });
+  let tempObj = {
+    name: productInDb.name,
+    price: productInDb.price,
+    quantity: 1 // Change to html-input so users can add more than 1 items to cart?
+  };
 };
 
 const getTotalPrice = () => {
