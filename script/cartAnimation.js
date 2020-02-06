146a943426623356774d038789507c09eac7824c
Samuel Mårtensson <samuel137@live.se>
2020-02-06 18:26:24 +0100

    added new cart animation

--- script/cartAnimation.js
+++ script/cartAnimation.js
@@ -1,32 +1,45 @@
-const cartContainer = document.querySelector(".cart-fixed");
-const hideBtn = document.querySelector(".cart-fixed__name-total-wrap");
-cartContainer.style = "transform: translateY(0px)";
+const cartContainer = document.querySelector('.cart-fixed');
+const hideBtn = document.querySelector('.cart-fixed__name-total-wrap');
+cartContainer.style = 'transform: translateY(0px)';
 
-hideBtn.addEventListener("click", function() {
-  if (cartContainer.style.transform != "translateY(0px)") {
-    cartContainer.style = "transform: translateY(0px)";
+hideBtn.addEventListener('click', function() {
+  if (cartContainer.style.transform !== 'translate(0px, 0px)') {
+    cartContainer.style = `transform: translate(0px ,${cartContainer.clientHeight -
+      60}px)`;
+    setTimeout(() => {
+      cartContainer.style = `transform: translate(0px, 0px)`;
+    }, 200);
   } else {
     cartContainer.style =
-      "transform: translateY(" + (cartContainer.clientHeight - 60) + "px)";
+      'transform: translateY(' + (cartContainer.clientHeight - 60) + 'px)';
+    setTimeout(() => {
+      cartContainer.style = `transform: translate(${cartContainer.clientWidth -
+        60}px, ${cartContainer.clientHeight - 60}px)`;
+    }, 200);
   }
 });
 
 // Makes sure cartContainer does not stick up more than it should when cartContainer item list gets taller
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
   let addBtns;
 
   cartContainer.style =
-    "transform: translateY(" + (cartContainer.clientHeight - 60) + "px)";
+    'transform: translateY(' + (cartContainer.clientHeight - 60) + 'px)';
   setTimeout(() => {
-    addBtns = document.querySelectorAll(".product-card__buy-btn");
+    addBtns = document.querySelectorAll('.product-card__buy-btn');
 
     addBtns.forEach(btn => {
-      btn.addEventListener("click", function() {
-        if (cartContainer.style.transform != "translateY(0px)") {
+      btn.addEventListener('click', function() {
+        if (cartContainer.style.transform != 'translateY(0px)') {
+          cartContainer.style.transitionDuration = '0';
           cartContainer.style =
-            "transform: translateY(" +
+            'transform: translateY(' +
             (cartContainer.clientHeight - 60) +
-            "px)";
+            'px)';
+          setTimeout(() => {
+            cartContainer.style = `transform: translate(${cartContainer.clientWidth -
+              60}px, ${cartContainer.clientHeight - 60}px)`;
+          }, 200);
         }
       });
     });
